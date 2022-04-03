ba5844896d97bdd90eb326bb5742089dd25a9d8b
Aakif Akhtar <san.akhtar7787@gmail.com>
2022-04-03 11:43:38 +0530

    commit change

--- chem.py
+++ chem.py
@@ -6,7 +6,7 @@ from pennylane.ops.qubit import PauliX, PauliZ
 from qiskit import *
 from pennylane import numpy as np
 
-class Finance:
+class chemsitry:
     def __init__(self, time_step, total_qubit):
         self.time_step = time_step
         self.total_qubit = total_qubit
@@ -14,5 +14,14 @@ class Finance:
         self.cost_history = []
 
 
-    def Create_Gp(self):
+    def GHZ_state_prep(self):
+        qml.Hadamard(wires=2)
+        qml.CNOT(wires=(2,3))
+        qml.SWAP(wires = (1,3))
+    
+    def givens_rotation(self, wire_index):
+        qml.IsingXX(wires= (wire_index, wire_index+1))
+        qml.IsingYY(wires=(wire_index, wire_index +1))
+
+    def perfec_pairing(self):
         pass
